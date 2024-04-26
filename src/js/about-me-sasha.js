6b1472044eb18e3fa42d86992d77a4887f2463d0
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-04-26 19:20:24 +0200

    add js

--- src/js/about-me-sasha.js
+++ src/js/about-me-sasha.js
@@ -1,14 +1,5 @@
 import Accordion from 'accordion-js';
 
-document.querySelectorAll('.about-arrow').forEach(arrow => {
-  arrow.addEventListener('click', function () {
-    const container = this.parentElement; // Батьківський елемент '.about-container'
-    container.classList.toggle('active'); // Перемикання класу 'active'
-    const panel = container.nextElementSibling; // Пошук наступного елемента, який є текстовим списком
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
-    } else {
-      panel.style.display = 'block';
-    }
-  });
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   new Accordion('.accordion-container');
+// });
