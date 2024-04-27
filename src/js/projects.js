795f84ed90a03c9f87c9ae3afb9bc671a4716917
Artur <agavrik464@gmail.com>
2024-04-27 21:25:38 +0300

    added swiper

---
+++ src/js/projects.js
@@ -0,0 +1,13 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
