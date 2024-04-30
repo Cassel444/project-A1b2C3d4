7dbf976356aa385bf200a7edbae83b47fb98aae5
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-04-30 17:39:39 +0200

    fix arrow and js

--- src/js/about-me-sasha.js
+++ src/js/about-me-sasha.js
@@ -2,27 +2,11 @@ import Accordion from 'accordion-js';
 
 document.addEventListener('DOMContentLoaded', function () {
   new Accordion('.about-accordion-container', {
-    duration: 500,
-    ariaEnabled: true,
-    collapse: true,
-    showMultiple: false,
-    onlyChildNodes: true,
+    duration: 600,
     openOnInit: [0],
-    elementClass: 'about-ac', // оновлений клас елементів
-    triggerClass: 'about-ac-trigger', // оновлений клас кнопки тригера
-    panelClass: 'about-ac-panel', // оновлений клас панелі
-    activeClass: 'about-is-active', // оновлений клас активного стану
-    beforeOpen: currElement => {
-      console.log('Перед відкриттям:', currElement);
-    },
-    onOpen: currElement => {
-      console.log('Відкрито:', currElement);
-    },
-    beforeClose: currElement => {
-      console.log('Перед закриттям:', currElement);
-    },
-    onClose: currElement => {
-      console.log('Закрито:', currElement);
-    },
+    elementClass: 'about-ac',
+    triggerClass: 'about-ac-trigger',
+    panelClass: 'about-ac-panel',
+    activeClass: 'about-is-active',
   });
 });
