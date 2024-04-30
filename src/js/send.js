e10f0ec5e8b135893196715b40288b40b0cc0bcf
t-matveentsev <tmatveentsev@gmail.com>
2024-04-30 13:20:17 +0200

    js-done/animation

--- src/js/send.js
+++ src/js/send.js
@@ -103,3 +103,27 @@ function onInputChange() {
   };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(savedForm));
 }
+
+const footerContacts = document.querySelectorAll('.footer-contact');
+
+footerContacts.forEach(contact => {
+  contact.addEventListener('mousemove', e => {
+    const { offsetX, offsetY } = e;
+    const { offsetWidth, offsetHeight } = contact;
+
+    const centerX = offsetWidth / 2;
+    const centerY = offsetHeight / 2;
+
+    const deltaX = (offsetX - centerX) / centerX;
+    const deltaY = (offsetY - centerY) / centerY;
+
+    const shadowX = deltaX * 20;
+    const shadowY = deltaY * 20;
+
+    contact.style.boxShadow = `${shadowX}px ${shadowY}px 30px rgba(166, 166, 166, 0.2)`;
+  });
+
+  contact.addEventListener('mouseleave', () => {
+    contact.style.boxShadow = 'none';
+  });
+});
