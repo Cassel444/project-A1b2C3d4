54c9d725558d32adbfd00b87e71325f19d135011
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-04-30 19:37:05 +0200

    Update aos.js

--- src/js/aos.js
+++ src/js/aos.js
@@ -1,4 +1,8 @@
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1000,
+  once: true,
+  mirror: false,
+});
