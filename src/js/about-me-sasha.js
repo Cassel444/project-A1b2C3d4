55b47012a343636ecfd87be80dbea9388c6e4951
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-04-30 16:35:35 +0200

    change class name

--- src/js/about-me-sasha.js
+++ src/js/about-me-sasha.js
@@ -1,9 +1,28 @@
 import Accordion from 'accordion-js';
 
 document.addEventListener('DOMContentLoaded', function () {
-  new Accordion('.accordion-container', {
-    showMultiple: false,
-    openOnInit: [0],
+  new Accordion('.about-accordion-container', {
     duration: 500,
+    ariaEnabled: true,
+    collapse: true,
+    showMultiple: false,
+    onlyChildNodes: true,
+    openOnInit: [0],
+    elementClass: 'about-ac', // оновлений клас елементів
+    triggerClass: 'about-ac-trigger', // оновлений клас кнопки тригера
+    panelClass: 'about-ac-panel', // оновлений клас панелі
+    activeClass: 'about-is-active', // оновлений клас активного стану
+    beforeOpen: currElement => {
+      console.log('Перед відкриттям:', currElement);
+    },
+    onOpen: currElement => {
+      console.log('Відкрито:', currElement);
+    },
+    beforeClose: currElement => {
+      console.log('Перед закриттям:', currElement);
+    },
+    onClose: currElement => {
+      console.log('Закрито:', currElement);
+    },
   });
 });
